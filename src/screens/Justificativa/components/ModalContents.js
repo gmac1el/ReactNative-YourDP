0cab33f4ce470e3fe5e7cd5646270d26c29867cc
Caliel-Albuquerque <75955275+Caliel-Albuquerque@users.noreply.github.com>
2023-08-05 23:19:05 -0300

    justificativa

---
+++ src/screens/Justificativa/components/ModalContents.js
@@ -0,0 +1,215 @@
+import React, { useState } from "react";
+
+import { View, Text, Button, StyleSheet, ScrollView, TouchableOpacity, Modal, SafeAreaView, KeyboardAvoidingView, Platform, Alert } from 'react-native';
+
+import * as ImagePicker from 'expo-image-picker';
+//import * as Permissions from 'expo-permissions'; //
+import * as MediaLibrary from 'expo-media-library';
+
+import { TextInput } from 'react-native-paper';
+import axios from 'axios';
+
+
+
+
+export default function ModalContent() {
+
+  const [motivo, setMotivo] = useState('');
+  const [data, setData] = useState('');
+  const [observacao, setObservacao] = useState('');
+  const [modalVisible, setModalVisible] = useState(false);
+
+
+  const handleSubmit = () => {
+    console.log('Motivo:', motivo);
+    console.log('Data:', data);
+    console.log('Observação:', observacao);
+  };
+
+  const handleUploadImage = () => {
+    console.log('Imagem enviada');
+  };
+
+  const [userData, setUserData] = useState(null);
+
+  const handleUserDataLoaded = (data) => {
+    setUserData(data.msg.ponto);
+  };
+
+
+  const pickFromGalary = async () => {
+    // Request permission to access the camera roll
+    const { status } = await MediaLibrary.requestPermissionsAsync();
+
+    if (status === 'granted') {
+      // If permission is granted, launch the image picker
+      let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        allowsEditing: true,
+        aspect: [1, 1],
+        quality: 0.5,
+      });
+
+      if (!result.cancelled) {
+        // Use the "assets" array instead of "uri" to access selected asset(s)
+        let selectedAsset = result.assets[0]; // Assuming only one asset is selected
+
+        let newFile = {
+          uri: selectedAsset.uri,
+          type: `test/${selectedAsset.uri.split(".")[1]}`,
+          name: `test.${selectedAsset.uri.split(".")[1]}`,
+        };
+        handleUpload(newFile);
+      }
+    } else {
+      Alert.alert('Você deve dar permissão para executar');
+    }
+  };
+
+  const handleUpload = (image) => {
+    const data = new FormData(); // fetch ucun data hazirlayiriq
+    data.append('file', image);  // cloudinary ucun img file,hansiki imgpicker den gelir
+    data.append('upload_preset', 'ml_default'); // cloudinary ucun preset ti teyin edirik
+    data.append('cloud_name', 'dkt07q4bz'); // cloudinary ucun cloud ad
+    fetch("https://api.cloudinary.com/v1_1/dkt07q4bz/image/upload", { method: 'post', body: data })
+      .then(res => res.json())
+      .then(data => { inputHndl('picture', data.url); }); // cloudinary yuklenenden sora gelen datani state guncelliyirik,data cloudinaryden gelir
+  }
+
+
+
+  return (
+    <View style={styles.form}>
+
+      <KeyboardAvoidingView
+        style={styles.container}
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'} // Define o comportamento para iOS e Android
+        keyboardVerticalOffset={Platform.OS === 'ios' ? 64 : 0} // Offset vertical (opcional)
+      >
+
+      </KeyboardAvoidingView>
+      <SafeAreaView style={styles.form}>
+
+        <View style={styles.motivoSection}>
+          {/* <Text style={styles.label}>Justificativa</Text> */}
+          <TextInput
+            style={styles.inputAusencia}
+            value={motivo}
+            onChangeText={setMotivo}
+            label='Motivo'
+            mode='outlined'
+          />
+        </View>
+
+        <View style={styles.inputDateFerias}>
+          {/* <Text style={styles.label}>Data de Solicitação</Text> */}
+          <TextInput
+            style={styles.inputDate}
+            value={data}
+            onChangeText={setData}
+            label="Data"
+            mode='outlined'
+          />
+        </View>
+
+        <View style={styles.observation}>
+          {/* <Text style={styles.label}>Observação</Text> */}
+          <TextInput
+            style={styles.subject}
+            value={observacao}
+            onChangeText={setObservacao}
+
+            multiline
+            label="Observação"
+            mode='outlined'
+          />
+        </View>
+
+        <TouchableOpacity style={styles.search} onPress={pickFromGalary}>
+          <Text style={[styles.btnText, { color: '#2D1CC6' }]}>Subir imagem</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.btn} onPress={handleSubmit}>
+          <Text style={styles.btnText}>Enviar</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+
+    </View>
+  )
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  form: {
+    flexDirection: 'column',
+    gap: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '85%',
+    zIndex: 1000
+  },
+  motivoSection: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 10,
+    width: '100%',
+  },
+  inputAusencia: {
+    backgroundColor: '#F7F7F9',
+    borderRadius: 14,
+    width: '100%',
+    height: 56,
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  inputDateFerias: {
+    width: '100%',
+    marginBottom: 10,
+  },
+  subject: {
+    width: '100%',
+    height: 200,
+
+    borderColor: '#ccc',
+    borderRadius: 14,
+    paddingHorizontal: 10,
+    paddingTop: 10,
+    textAlignVertical: 'top',
+  },
+  search: {
+    width: 335,
+    height: 56,
+    backgroundColor: '#F7F7F9',
+    borderRadius: 14,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+    marginTop: 20
+  },
+  btn: {
+    width: 335,
+    height: 56,
+    padding: 10,
+    borderRadius: 16,
+    backgroundColor: '#2D1CC6',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  btnText: {
+    color: '#FFF',
+    fontSize: 26,
+    fontWeight: '700',
+  },
+  observation: {
+    width: '100%'
+  }
+})
--- src/screens/Justificativa/index.js
+++ src/screens/Justificativa/index.js
@@ -1,14 +1,20 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
+import { View, Text, Button, StyleSheet, ScrollView, TouchableOpacity, Modal, SafeAreaView, KeyboardAvoidingView, Platform } from 'react-native';
 
+import { FAB } from 'react-native-paper';
+import { TextInput } from 'react-native-paper';
 import { TopBar } from '../../components/Topbar';
 
+import ModalContent from './components/ModalContents';
+
 import { Container } from '../Styles';
+
 
 const JustificativaForm = () => {
   const [motivo, setMotivo] = useState('');
   const [data, setData] = useState('');
   const [observacao, setObservacao] = useState('');
+  const [modalVisible, setModalVisible] = useState(false);
 
   const handleSubmit = () => {
     console.log('Motivo:', motivo);
@@ -26,6 +32,8 @@ const JustificativaForm = () => {
     setUserData(data.msg.ponto);
   };
 
+
+
   return (
 
 
@@ -33,55 +41,53 @@ const JustificativaForm = () => {
       <ScrollView stickyHeaderIndices={[0]} showsVerticalScrollIndicator={false} contentContainerStyle={styles.scrollStyle} style={{ width: '100%' }}>
         <TopBar onUserDataLoaded={handleUserDataLoaded} />
 
-        <View style={styles.container}>
-          <View style={styles.form}>
-            <View style={styles.motivoSection}>
-              <Text style={styles.label}>Justificativa</Text>
-              <TextInput
-                style={styles.inputAusencia}
-                value={motivo}
-                onChangeText={setMotivo}
-                placeholder="Motivo"
-              />
-            </View>
 
-            <View style={styles.inputDateFerias}>
-              <Text style={styles.label}>Data de Solicitação</Text>
-              <TextInput
-                style={styles.inputDate}
-                value={data}
-                onChangeText={setData}
-                placeholder="Data"
-              />
-            </View>
+        <Modal
+          visible={modalVisible}
+          animationType="slide"
+          onRequestClose={() => setModalVisible(false)}
 
-            <View style={styles.observation}>
-              <Text style={styles.label}>Observação</Text>
-              <TextInput
-                style={styles.subject}
-                value={observacao}
-                onChangeText={setObservacao}
-                placeholder="Digite aqui..."
-                multiline
-              />
-            </View>
+        >
 
-            <TouchableOpacity style={styles.search} onPress={handleUploadImage}>
-              <Text style={styles.btnText}>Subir imagem</Text>
-            </TouchableOpacity>
+          <View style={styles.modalContainer}>
 
-            <TouchableOpacity style={styles.btn} onPress={handleSubmit}>
-              <Text style={styles.btnText}>Enviar</Text>
+            <ModalContent />
+
+            <TouchableOpacity style={styles.iconModal} title="Fechar Modal" onPress={() => setModalVisible(false)}>
+              <Text style={[styles.textModal, {fontWeight: 700, fontSize: 25, marginRight: 10}]}>x</Text>
             </TouchableOpacity>
           </View>
-        </View>
+
+          {/* <View style={styles.container}>
+          
+          </View> */}
+
+
+        </Modal>
+
+
+
+
+
 
       </ScrollView>
+
+
+      <FAB
+        icon="plus"
+        style={styles.fab}
+        onPress={() => setModalVisible(true)}
+        label='Adicionar Ausência'
+        color='#ffff'
+
+      />
 
     </Container>
 
 
   );
+
+
 };
 
 export default JustificativaForm;
@@ -93,77 +99,23 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     padding: 20,
   },
-  form: {
-    width: '100%',
+  fab: {
+    position: 'absolute',
+    margin: 16,
+    right: 0,
+    bottom: 0,
+    backgroundColor: '#2D1CC6'
   },
-  motivoSection: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    gap: 10,
-    width: '100%',
-  },
-  inputAusencia: {
-    backgroundColor: '#F7F7F9',
-    borderRadius: 14,
-    width: '100%',
-    height: 56,
-    paddingLeft: 10,
-    paddingRight: 10,
-  },
-  inputDateFerias: {
-    width: '100%',
-    marginBottom: 10,
-  },
-  inputDate: {
-    backgroundColor: '#F7F7F9',
-    borderRadius: 14,
-    width: '100%',
-    height: 56,
-    paddingLeft: 10,
-    paddingRight: 10,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-around',
-  },
-  observation: {
-    width: '100%',
-    marginBottom: 10,
-  },
-  subject: {
-    width: '100%',
-    height: 200,
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 14,
-    paddingHorizontal: 10,
-    paddingTop: 10,
-    textAlignVertical: 'top',
-  },
-  search: {
-    width: 335,
-    height: 56,
-    backgroundColor: '#F7F7F9',
-    borderRadius: 14,
+  modalContainer: {
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    marginBottom: 10,
+    backgroundColor: '#fff'
   },
-  btn: {
-    width: 335,
-    height: 56,
-    padding: 10,
-    borderRadius: 16,
-    backgroundColor: '#2D1CC6',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginBottom: 10,
+  iconModal: {
+    position: 'absolute',
+    top: -40,
+    right: 0,
+    padding: 50,
   },
-  btnText: {
-    color: '#FFF',
-    fontSize: 26,
-    fontWeight: '700',
-  },
-
 });
